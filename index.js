5148ff33d0bee33c8bca8aaa04ad29e799baa1c4
Lena Hou <lenahou2013@gmail.com>
2020-12-03 15:55:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  switch(string){
+  case 'string':
+  return "I can't hear you!";
+  break;
+  case 'STRING' {
+    return "YES INDEED!";
+    break;
+    case "I love you, Grandma.":
+    return "I love you, too.";
+    break;
+  }
+}
+}
