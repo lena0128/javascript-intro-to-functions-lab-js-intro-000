22e3b2b958753ecceb028f492f6388024a2ed3d4
Lena Hou <lenahou2013@gmail.com>
2020-12-03 15:56:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   switch(string){
-  case 'string':
+  case 'string.toLowerCase()':
   return "I can't hear you!";
   break;
-  case 'STRING':
+  case 'string.toUpperCase()':
     return "YES INDEED!";
     break;
     case "I love you, Grandma.":
